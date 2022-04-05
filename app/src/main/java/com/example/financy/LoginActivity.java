83ca3ac3d898903db3bf748a7aa8034383d5cd86
SameerFaisal7 <mf04709@st.habib.edu.pk>
2022-04-06 02:52:06 +0500

    Financy

---
+++ app/src/main/java/com/example/financy/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.financy;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button loginBtn;
+    private TextView loginQn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        loginQn = findViewById(R.id.loginQn);
+
+
+    }
+}
