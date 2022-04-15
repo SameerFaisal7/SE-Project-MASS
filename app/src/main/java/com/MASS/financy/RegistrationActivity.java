7c5283e41c91360fb136a5044c24be777f5830d1
AliUrRehman <55151452+AliUrRehman@users.noreply.github.com>
2022-04-15 18:53:45 +0500

    Add files via upload

---
+++ app/src/main/java/com/MASS/financy/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.MASS.financy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.MASS.financy.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
