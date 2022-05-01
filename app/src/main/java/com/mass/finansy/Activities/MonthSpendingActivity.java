e569efeb9bfe52f89ef2149eab103865fdc63f39
AliUrRehman <55151452+AliUrRehman@users.noreply.github.com>
2022-05-01 22:05:08 +0500

    Add files via upload

---
+++ app/src/main/java/com/mass/finansy/Activities/MonthSpendingActivity.java
@@ -0,0 +1,16 @@
+package com.mass.finansy.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mass.finansy.R;
+
+public class MonthSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_spending);
+    }
+}
